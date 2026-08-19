4339b983b40021449d24e80d3e066031a439342c
IsraelDXPP <nydilupi75@hotmail.com>
2026-08-18 19:57:00 -0700

    Add advanced settings page with resource pack switching to NewOptionsDialog

--- src/Lawn/Widget/NewOptionsDialog.cpp
+++ src/Lawn/Widget/NewOptionsDialog.cpp
@@ -30,21 +30,26 @@
 #include "NewOptionsDialog.h"
 #include "../../ConstEnums.h"
 #include "../../PvzpLib/PvzpFoley.h"
+#include "../../PvzpLib/PvzpStringFile.h"
 #include "widget/Slider.h"
 #include "widget/Checkbox.h"
-#include "../../PvzpLib/PvzpStringFile.h"
 
 using namespace Sexy;
 
-NewOptionsDialog::NewOptionsDialog(LawnApp* theApp, bool theFromGameSelector) :
+const Color cOptionsTextColor(107, 109, 145);
+
+NewOptionsDialog::NewOptionsDialog(LawnApp* theApp, bool theFromGameSelector, bool theAdvanced) :
 	Dialog(nullptr, nullptr, Dialogs::DIALOG_NEWOPTIONS, true, "Options", "", "", Dialog::BUTTONS_NONE)
 {
 	mApp = theApp;
 	mFromGameSelector = theFromGameSelector;
+	mAdvancedMode = theAdvanced;
+	mAdvancedPage = theAdvanced ? 1 : 0;
 	SetColor(Dialog::COLOR_BUTTON_TEXT, Color(255, 255, 100));
 	mAlmanacButton = MakeButton(NewOptionsDialog::NewOptionsDialog_Almanac, this, "[VIEW_ALMANAC_BUTTON]");
 	mRestartButton = MakeButton(NewOptionsDialog::NewOptionsDialog_Restart, this, "[RESTART_LEVEL]");
 	mBackToMainButton = MakeButton(NewOptionsDialog::NewOptionsDialog_MainMenu, this, "[MAIN_MENU_BUTTON]");
+	mAdvancedButton = MakeButton(NewOptionsDialog::NewOptionsDialog_Advanced, this, "[ADVANCED_OPTIONS_BUTTON]");
 
 	mBackToGameButton = MakeNewButton(
 		Dialog::ID_OK,
@@ -77,6 +82,26 @@ NewOptionsDialog::NewOptionsDialog(LawnApp* theApp, bool theFromGameSelector) :
 	mFullscreenCheckbox = MakeNewCheckbox(NewOptionsDialog::NewOptionsDialog_Fullscreen, this, !theApp->mIsWindowed);
 	mHardwareAccelerationCheckbox = MakeNewCheckbox(NewOptionsDialog::NewOptionsDialog_HardwareAcceleration, this, theApp->Is3DAccelerated());
 
+	mLeftPageButton = MakeNewButton(NewOptionsDialog::NewOptionsDialog_LeftPage, this, "", nullptr, IMAGE_ZOMBATAR_PREV_BUTTON,
+		IMAGE_ZOMBATAR_PREV_BUTTON_HIGHLIGHT, IMAGE_ZOMBATAR_PREV_BUTTON_HIGHLIGHT);
+	mLeftPageButton->SetVisible(false);
+
+	mRightPageButton = MakeNewButton(NewOptionsDialog::NewOptionsDialog_RightPage, this, "", nullptr, IMAGE_ZOMBATAR_NEXT_BUTTON,
+		IMAGE_ZOMBATAR_NEXT_BUTTON_HIGHLIGHT, IMAGE_ZOMBATAR_NEXT_BUTTON_HIGHLIGHT);
+	mRightPageButton->SetVisible(false);
+
+	mReloadResourcePacksButton = MakeButton(NewOptionsDialog::NewOptionsDialog_ReloadResourcePacks, this, "[OPTIONS_RELOAD_RESOURCE_PACKS]");
+	mReloadResourcePacksButton->SetVisible(false);
+
+	mResourcePackButton = MakeNewButton(NewOptionsDialog::NewOptionsDialog_ResourcePack, this, mApp->GetResourcePackString(), nullptr,
+		IMAGE_BLANK, IMAGE_BLANK, IMAGE_BLANK);
+	mResourcePackButton->SetFont(FONT_DWARVENTODCRAFT18);
+	mResourcePackButton->mColors[ButtonWidget::COLOR_LABEL] = cOptionsTextColor;
+	mResourcePackButton->mColors[ButtonWidget::COLOR_LABEL_HILITE] = Color(1, 233, 1);
+	mResourcePackButton->SetVisible(false);
+
+	mAdvancedButton->SetVisible(false);
+
 	if (mFromGameSelector)
 	{
 		mRestartButton->SetVisible(false);
@@ -89,6 +114,20 @@ NewOptionsDialog::NewOptionsDialog(LawnApp* theApp, bool theFromGameSelector) :
 		{
 			mBackToMainButton->SetVisible(false);
 		}
+	}
+	else
+	{
+		mAdvancedButton->SetVisible(!theAdvanced);
+	}
+
+	if (mAdvancedMode)
+	{
+		mRestartButton->SetVisible(false);
+		mAlmanacButton->SetVisible(false);
+		mBackToMainButton->SetVisible(false);
+		mAdvancedButton->SetVisible(false);
+		mBackToGameButton->SetLabel("[DIALOG_BUTTON_BACK]");
+		mBackToGameButton->mId = NewOptionsDialog::NewOptionsDialog_Back;
 	}
 
 	if (mApp->mGameMode == GameMode::GAMEMODE_CHALLENGE_ICE ||
@@ -109,6 +148,10 @@ NewOptionsDialog::NewOptionsDialog(LawnApp* theApp, bool theFromGameSelector) :
 	{
 		mAlmanacButton->SetVisible(false);
 	}
+	if ((!mRestartButton->mVisible || !mAlmanacButton->mVisible) && !mFromGameSelector && !mAdvancedMode)
+	{
+		mAdvancedButton->SetVisible(true);
+	}
 }
 
 NewOptionsDialog::~NewOptionsDialog()
@@ -120,6 +163,11 @@ NewOptionsDialog::~NewOptionsDialog()
 	delete mAlmanacButton;
 	delete mRestartButton;
 	delete mBackToMainButton;
+	delete mAdvancedButton;
+	delete mLeftPageButton;
+	delete mRightPageButton;
+	delete mReloadResourcePacksButton;
+	delete mResourcePackButton;
 	delete mBackToGameButton;
 }
 
@@ -135,11 +183,16 @@ void NewOptionsDialog::AddedToManager(Sexy::WidgetManager* theWidgetManager)
 	AddWidget(mAlmanacButton);
 	AddWidget(mRestartButton);
 	AddWidget(mBackToMainButton);
+	AddWidget(mAdvancedButton);
 	AddWidget(mMusicVolumeSlider);
 	AddWidget(mSfxVolumeSlider);
 	AddWidget(mHardwareAccelerationCheckbox);
 	AddWidget(mFullscreenCheckbox);
 	AddWidget(mBackToGameButton);
+	AddWidget(mLeftPageButton);
+	AddWidget(mRightPageButton);
+	AddWidget(mReloadResourcePacksButton);
+	AddWidget(mResourcePackButton);
 }
 
 void NewOptionsDialog::RemovedFromManager(Sexy::WidgetManager* theWidgetManager)
@@ -148,11 +201,15 @@ void NewOptionsDialog::RemovedFromManager(Sexy::WidgetManager* theWidgetManager)
 	RemoveWidget(mAlmanacButton);
 	RemoveWidget(mMusicVolumeSlider);
 	RemoveWidget(mSfxVolumeSlider);
-	RemoveWidget(mFullscreenCheckbox);
 	RemoveWidget(mHardwareAccelerationCheckbox);
+	RemoveWidget(mFullscreenCheckbox);
 	RemoveWidget(mBackToMainButton);
-	RemoveWidget(mBackToGameButton);
 	RemoveWidget(mRestartButton);
+	RemoveWidget(mAdvancedButton);
+	RemoveWidget(mLeftPageButton);
+	RemoveWidget(mRightPageButton);
+	RemoveWidget(mReloadResourcePacksButton);
+	RemoveWidget(mResourcePackButton);
 }
 
 void NewOptionsDialog::Resize(int theX, int theY, int theWidth, int theHeight)
@@ -165,7 +222,19 @@ void NewOptionsDialog::Resize(int theX, int theY, int theWidth, int theHeight)
 	mAlmanacButton->Resize(107, 241, 209, 46);
 	mRestartButton->Resize(mAlmanacButton->mX, mAlmanacButton->mY + 43, 209, 46);
 	mBackToMainButton->Resize(mRestartButton->mX, mRestartButton->mY + 43, 209, 46);
+	mAdvancedButton->Resize(mRestartButton->mX, mRestartButton->mY + 43, 209, 46);
 	mBackToGameButton->Resize(30, 381, mBackToGameButton->mWidth, mBackToGameButton->mHeight);
+	mLeftPageButton->Resize(100, 330, IMAGE_ZOMBATAR_PREV_BUTTON->mWidth, IMAGE_ZOMBATAR_PREV_BUTTON->mHeight);
+	mRightPageButton->Resize(280, 330, IMAGE_ZOMBATAR_NEXT_BUTTON->mWidth, IMAGE_ZOMBATAR_NEXT_BUTTON->mHeight);
+	mReloadResourcePacksButton->Resize(mWidth / 2 - 130, 160, 260, 46);
+	mResourcePackButton->Resize(mWidth / 2 + 15, mReloadResourcePacksButton->mY + 50, 0, FONT_DWARVENTODCRAFT18->GetHeight());
+	ResizeResourcePackButton();
+
+	if ((!mRestartButton->mVisible || !mAlmanacButton->mVisible) && !mFromGameSelector && !mAdvancedMode)
+	{
+		LawnStoneButton* button = mRestartButton->mVisible ? mRestartButton : mAlmanacButton;
+		mAdvancedButton->Resize(button->mX, button->mY, button->mWidth, button->mHeight);
+	}
 
 	if (mFromGameSelector)
 	{
@@ -173,6 +242,17 @@ void NewOptionsDialog::Resize(int theX, int theY, int theWidth, int theHeight)
 		mSfxVolumeSlider->mY += 10;
 		mHardwareAccelerationCheckbox->mY += 15;
 		mFullscreenCheckbox->mY += 20;
+	}
+
+	if (mAdvancedMode)
+	{
+		mMusicVolumeSlider->SetVisible(false);
+		mSfxVolumeSlider->SetVisible(false);
+		mHardwareAccelerationCheckbox->SetVisible(false);
+		mFullscreenCheckbox->SetVisible(false);
+		mLeftPageButton->SetVisible(true);
+		mRightPageButton->SetVisible(true);
+		UpdateAdvancedPage();
 	}
 
 	if (mApp->mGameMode == GameMode::GAMEMODE_CHALLENGE_ZEN_GARDEN || mApp->mGameMode == GameMode::GAMEMODE_TREE_OF_WISDOM)
@@ -196,19 +276,31 @@ void NewOptionsDialog::Draw(Sexy::Graphics* g)
 		a3DAccelOffset = 15;
 		aFullScreenOffset = 20;
 	}
-	Sexy::Color aTextColor(107, 109, 145);
 
-	int aSliderLabelsX = mApp->GetInteger("OPTION_DLG_SLIDER_LABELS_OFFSET_X", 186);
-	int aCheckboxLabelsX = mApp->GetInteger("OPTION_DLG_CHECKBOX_LABELS_OFFSET_X", 274);
-	float aFontScale = static_cast<float>(mApp->GetDouble("OPTION_DLG_LABEL_FONT_SCALE", 1.0));
-	if (aFontScale != 1.0f)
-		g->SetScale(aFontScale, aFontScale, 0.0f, 0.0f);
-	PvzpDrawString(g, mApp->GetString("OPTIONS_MUSIC_LABEL", "Music"), aSliderLabelsX, 140 + aMusicOffset, FONT_DWARVENTODCRAFT18, aTextColor, DrawStringJustification::DS_ALIGN_RIGHT);
-	PvzpDrawString(g, mApp->GetString("OPTIONS_SOUNDFX", "Sound FX"), aSliderLabelsX, 167 + aSfxOffset, FONT_DWARVENTODCRAFT18, aTextColor, DrawStringJustification::DS_ALIGN_RIGHT);
-	PvzpDrawString(g, mApp->GetString("OPTIONS_3D_ACCELERATION", "3D Acceleration"), aCheckboxLabelsX, 197 + a3DAccelOffset, FONT_DWARVENTODCRAFT18, aTextColor, DrawStringJustification::DS_ALIGN_RIGHT);
-	PvzpDrawString(g, mApp->GetString("OPTIONS_FULL_SCREEN", "Full Screen"), aCheckboxLabelsX, 229 + aFullScreenOffset, FONT_DWARVENTODCRAFT18, aTextColor, DrawStringJustification::DS_ALIGN_RIGHT);
-	if (aFontScale != 1.0f)
-		g->SetScale(1.0f, 1.0f, 0.0f, 0.0f);
+	if (!mAdvancedMode)
+	{
+		float aFontScale = static_cast<float>(mApp->GetDouble("OPTION_DLG_LABEL_FONT_SCALE", 1.0));
+		int aSliderLabelsX = mApp->GetInteger("OPTION_DLG_SLIDER_LABELS_OFFSET_X", 186);
+		int aCheckboxLabelsX = mApp->GetInteger("OPTION_DLG_CHECKBOX_LABELS_OFFSET_X", 274);
+		if (aFontScale != 1.0f)
+			g->SetScale(aFontScale, aFontScale, 0.0f, 0.0f);
+		PvzpDrawString(g, mApp->GetString("OPTIONS_MUSIC_LABEL", "Music"), aSliderLabelsX, 140 + aMusicOffset, FONT_DWARVENTODCRAFT18, cOptionsTextColor, DrawStringJustification::DS_ALIGN_RIGHT);
+		PvzpDrawString(g, mApp->GetString("OPTIONS_SOUNDFX", "Sound FX"), aSliderLabelsX, 167 + aSfxOffset, FONT_DWARVENTODCRAFT18, cOptionsTextColor, DrawStringJustification::DS_ALIGN_RIGHT);
+		PvzpDrawString(g, mApp->GetString("OPTIONS_3D_ACCELERATION", "3D Acceleration"), aCheckboxLabelsX, 197 + a3DAccelOffset, FONT_DWARVENTODCRAFT18, cOptionsTextColor, DrawStringJustification::DS_ALIGN_RIGHT);
+		PvzpDrawString(g, mApp->GetString("OPTIONS_FULL_SCREEN", "Full Screen"), aCheckboxLabelsX, 229 + aFullScreenOffset, FONT_DWARVENTODCRAFT18, cOptionsTextColor, DrawStringJustification::DS_ALIGN_RIGHT);
+		if (aFontScale != 1.0f)
+			g->SetScale(1.0f, 1.0f, 0.0f, 0.0f);
+	}
+	else
+	{
+		switch (mAdvancedPage)
+		{
+		case 1:
+			PvzpDrawString(g, mApp->GetString("OPTIONS_RESOURCE_PACK", "Resource Pack"), mResourcePackButton->mX - 6, mResourcePackButton->mY + 23, FONT_DWARVENTODCRAFT18, cOptionsTextColor, DrawStringJustification::DS_ALIGN_RIGHT);
+			break;
+		}
+		PvzpDrawString(g, "Page " + std::to_string(mAdvancedPage) + "/1", mWidth / 2, 355, FONT_DWARVENTODCRAFT18GREENINSET, Color::White, DrawStringJustification::DS_ALIGN_CENTER);
+	}
 }
 
 void NewOptionsDialog::SliderVal(int theId, double theVal)
@@ -300,12 +392,39 @@ void NewOptionsDialog::KeyDown(Sexy::KeyCode theKey)
 
 	if (theKey == KeyCode::KEYCODE_SPACE || theKey == KeyCode::KEYCODE_RETURN)
 	{
-		Dialog::ButtonDepress(Dialog::ID_OK);
+		if (mAdvancedMode)
+			ButtonDepress(NewOptionsDialog::NewOptionsDialog_Back);
+		else
+			Dialog::ButtonDepress(Dialog::ID_OK);
 	}
 	else if (theKey == KeyCode::KEYCODE_ESCAPE)
 	{
 		Dialog::ButtonDepress(Dialog::ID_CANCEL);
 	}
+}
+
+void NewOptionsDialog::UpdateAdvancedPage()
+{
+	if (mAdvancedPage <= 1)
+		mLeftPageButton->SetVisible(false);
+	else
+		mLeftPageButton->SetVisible(true);
+
+	mReloadResourcePacksButton->SetVisible(false);
+	mResourcePackButton->SetVisible(false);
+
+	switch (mAdvancedPage)
+	{
+	case 1:
+		mReloadResourcePacksButton->SetVisible(true);
+		mResourcePackButton->SetVisible(true);
+		break;
+	}
+}
+
+void NewOptionsDialog::ResizeResourcePackButton()
+{
+	mResourcePackButton->Resize(mResourcePackButton->mX, mResourcePackButton->mY, mResourcePackButton->mFont->StringWidth(PvzpStringTranslate(mResourcePackButton->mLabel)), mResourcePackButton->mHeight);
 }
 
 void NewOptionsDialog::ButtonPress(int theId)
@@ -324,6 +443,14 @@ void NewOptionsDialog::ButtonDepress(int theId)
 	{
 		AlmanacDialog* aDialog = mApp->DoAlmanacDialog(SeedType::SEED_NONE, ZombieType::ZOMBIE_INVALID);
 		aDialog->WaitForResult(true);
+		break;
+	}
+
+	case NewOptionsDialog::NewOptionsDialog_Advanced:
+	{
+		mApp->KillNewOptionsDialog();
+		mApp->DoNewOptions(mFromGameSelector, true);
+		mApp->PlaySample(SOUND_BUTTONCLICK);
 		break;
 	}
 
@@ -397,6 +524,34 @@ void NewOptionsDialog::ButtonDepress(int theId)
 
 	case NewOptionsDialog::NewOptionsDialog_Update:
 		mApp->CheckForUpdates();
+		break;
+
+	case NewOptionsDialog::NewOptionsDialog_ReloadResourcePacks:
+		mApp->ReloadResourcePacks();
+		mResourcePackButton->mLabel = mApp->GetResourcePackString();
+		ResizeResourcePackButton();
+		break;
+
+	case NewOptionsDialog::NewOptionsDialog_ResourcePack:
+		mApp->SwitchResourcePack();
+		mResourcePackButton->mLabel = mApp->GetResourcePackString();
+		ResizeResourcePackButton();
+		break;
+
+	case NewOptionsDialog::NewOptionsDialog_LeftPage:
+		mAdvancedPage--;
+		UpdateAdvancedPage();
+		break;
+
+	case NewOptionsDialog::NewOptionsDialog_RightPage:
+		mAdvancedPage++;
+		UpdateAdvancedPage();
+		break;
+
+	case NewOptionsDialog::NewOptionsDialog_Back:
+		mApp->KillNewOptionsDialog();
+		mApp->DoNewOptions(mFromGameSelector, false);
+		mApp->PlaySample(SOUND_BUTTONCLICK);
 		break;
 	}
 }
